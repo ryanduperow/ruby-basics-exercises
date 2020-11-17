290bcf2ec62b8cbfb8b0a864d635c4e511b11f63
Ryan DuPerow <rkdupe@gmail.com>
2020-11-17 12:33:16 +0000

    Exercises completed on 11/17/20

---
+++ conditional_exercises/stoplight.rb
@@ -0,0 +1,14 @@
+# In the code below, stoplight is randomly assigned as 'green', 'yellow', or 'red'.
+# Write a case statement that prints "Go!" if stoplight equals 'green', "Slow down!" if stoplight equals 'yellow', and "Stop!" 
+# if stoplight equals 'red'.
+
+stoplight = ['green', 'yellow', 'red'].sample
+
+case stoplight
+when 'green'
+    puts "Go!"
+when 'yellow'
+    puts "Slow down!"
+else
+    puts "Stop!"
+end    
---
+++ string_exercises/create_string.rb
@@ -0,0 +1,9 @@
+# Create an empty string using the String class and assign it to a variable.
+
+empty_string = ''
+p empty_string
+
+# another way:
+
+empty_string2 = String.new
+p empty_string2
