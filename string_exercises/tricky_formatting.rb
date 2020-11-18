e1b5d8d4f39073fa72e28b97211bf4041d7cf026
Ryan DuPerow <rkdupe@gmail.com>
2020-11-18 14:14:10 +0000

    Exercises for 11/18/20

---
+++ string_exercises/tricky_formatting.rb
@@ -0,0 +1,8 @@
+# Using the following code, capitalize the value of state then print the modified value. 
+# Note that state should have the modified value both before and after you print it.
+
+state = 'tExAs'
+
+puts state.capitalize!
+
+puts state
