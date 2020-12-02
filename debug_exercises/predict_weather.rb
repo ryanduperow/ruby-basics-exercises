dc8c8c282c96cd4ababa4925ee8ce7cf79d0a058
Ryan DuPerow <rkdupe@gmail.com>
2020-12-02 13:26:25 +0000

    Exercises for 12/2/20

---
+++ debug_exercises/predict_weather.rb
@@ -0,0 +1,16 @@
+# Our predict_weather method should output a message indicating whether a sunny or cloudy day lies ahead. 
+# However, the output is the same every time the method is invoked. Why? Fix the code so that it behaves as expected.
+
+def predict_weather
+  sunshine = [true, false].sample
+
+  if sunshine
+    puts "Today's weather will be sunny!"
+  else
+    puts "Today's weather will be cloudy!"
+  end
+end
+
+predict_weather
+
+# true and false can't be a string.  Removed quotatoins and the program works.
---
+++ debug_exercises/read_error.rb
@@ -0,0 +1,14 @@
+# You come across the following code. What errors does it raise for the given examples and what exactly do the error messages mean?
+
+def find_first_nonzero_among(numbers)
+  numbers.each do |n|
+    return n if n.nonzero?
+  end
+end
+
+# Examples
+
+find_first_nonzero_among(0, 0, 1, 0, 2, 0)
+find_first_nonzero_among(1)
+
+# Error on line 11 because method is expecting 1 arugument, but given 6
